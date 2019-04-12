865745c936fa20cbae2434e71761c3ab18903402
Piyush Mehta <me@piyushmehta.com>
2019-04-12 17:32:40 +0530

    testing

--- khel/lib/main.dart
+++ khel/lib/main.dart
@@ -11,6 +11,8 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  List<String> activities = ['Food', 'Swimming'];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
