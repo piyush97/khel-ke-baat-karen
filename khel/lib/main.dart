361852f5a55bfd1cdc524e7ab7149fe3eb064ee6
Piyush Mehta <me@piyushmehta.com>
2019-04-12 14:21:02 +0530

    testing

--- khel/lib/main.dart
+++ khel/lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 
-main() {
+void main() => {
+  runApp(MyApp())
+};
 
-}
-
-class MyApp extends StatelessWidget{
-  build(context) {
-    return MaterialApp();
-  } 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('K2BK')),
+      ),
+    );
+  }
 }
