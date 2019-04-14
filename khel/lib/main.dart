ebed61be09f4bb0b970f8f95df42dc366406a2f5
Piyush Mehta <me@piyushmehta.com>
2019-04-14 23:36:35 +0530

    Parsing Route Data Manually

--- khel/lib/main.dart
+++ khel/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import './pages/auth.dart';
 import './pages/activites_admin.dart';
 import './pages/activites.dart';
+import './activities.dart';
 
 void main() {
   runApp(MyApp());
@@ -16,9 +17,24 @@ class MyApp extends StatelessWidget {
           brightness: Brightness.dark,
           primarySwatch: Colors.lime,
           secondaryHeaderColor: Colors.amber),
+      // home: AuthPage(),
       routes: {
         '/': (BuildContext context) => ActivitiesPage(),
         '/admin': (BuildContext context) => ActivitiesAdminPage(),
+      },
+      onGenerateRoute: (RouteSettings settings) {
+        final List<String> pathElements = settings.name.split('/');
+        if (pathElements[0] != '') {
+          return null;
+        }
+        if (pathElements[1] == 'product') {
+          final int index = int.parse(pathElements[2]);
+          return MaterialPageRoute(
+            builder: (BuildContext context) => ActivityPage(
+                activities[index]['title'], activities[index]['image']),
+          );
+        }
+        return null;
       },
     );
   }
