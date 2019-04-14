2fcd9994b99b8e8000dd54b68d9b2e10f5c43996
Piyush Mehta <me@piyushmehta.com>
2019-04-15 01:12:14 +0530

    Configuring text fields

--- khel/lib/pages/activity_create.dart
+++ khel/lib/pages/activity_create.dart
@@ -3,19 +3,19 @@ import 'package:flutter/material.dart';
 class ActivityCreatePage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    // TODO: implement createState
     return _ActivityCreatePageState();
   }
 }
 
 class _ActivityCreatePageState extends State<ActivityCreatePage> {
-  String titleValue = '';
+  String titleValue;
+  String descriptionValue;
+  double time;
   @override
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
         TextField(
-          autocorrect: true,
           autofocus: true,
           onChanged: (String value) {
             setState(() {
@@ -23,7 +23,24 @@ class _ActivityCreatePageState extends State<ActivityCreatePage> {
             });
           },
         ),
-        Text(titleValue)
+        TextField(
+          maxLines: 4,
+          onChanged: (String value) {
+            setState(() {
+              descriptionValue = value;
+            });
+          },
+        ),
+        TextField(
+          keyboardType: TextInputType.number,
+          autocorrect: true,
+          autofocus: true,
+          onChanged: (String value) {
+            setState(() {
+              time = double.parse(value);
+            });
+          },
+        ),
       ],
     );
   }
