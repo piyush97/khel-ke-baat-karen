933b0663185745d868ff399b8f2f07aa8af5e012
Piyush Mehta <me@piyushmehta.com>
2019-04-15 16:19:14 +0530

    Refactoring code

--- lib/widgets/activities/activities.dart
+++ lib/widgets/activities/activities.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import './time.dart';
+
+import './activity_card.dart';
 
 class Activities extends StatelessWidget {
   final List<Map<String, dynamic>> activities;
@@ -7,73 +8,12 @@ class Activities extends StatelessWidget {
     print('[Activities widget Constructor]');
   }
 
-  Widget _buildActivityItem(BuildContext context, int index) {
-    return Card(
-      child: Column(
-        children: <Widget>[
-          Image.asset(activities[index]['image'] ?? ''),
-          SizedBox(
-            height: 10.0,
-          ),
-          Container(
-            padding: EdgeInsets.only(top: 10.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: <Widget>[
-                Text(
-                  activities[index]['title'] ?? '',
-                  style: TextStyle(
-                      fontSize: 26.0,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'Oswald'),
-                ),
-                SizedBox(
-                  width: 8.0,
-                ),
-                TimeTag(activities[index]['time'].toString()),
-              ],
-            ),
-          ),
-          DecoratedBox(
-            decoration: BoxDecoration(
-                border: Border.all(color: Theme.of(context).accentColor),
-                borderRadius: BorderRadius.circular(6.0)),
-            child: Padding(
-              child: Text('Hurry Up!'),
-              padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.5),
-            ),
-          ),
-          ButtonBar(
-            alignment: MainAxisAlignment.center,
-            children: <Widget>[
-              IconButton(
-                icon: Icon(Icons.info),
-                color: Theme.of(context).accentColor,
-                onPressed: () => Navigator.pushNamed<bool>(
-                      context,
-                      '/activity/' + index.toString(),
-                    ),
-              ),
-              IconButton(
-                icon: Icon(Icons.favorite_border),
-                color: Colors.red,
-                onPressed: () => Navigator.pushNamed<bool>(
-                      context,
-                      '/activity/' + index.toString(),
-                    ),
-              )
-            ],
-          )
-        ],
-      ),
-    );
-  }
-
   Widget _buildActivityList() {
     Widget activityCards;
     if (activities.length > 0) {
       activityCards = ListView.builder(
-        itemBuilder: _buildActivityItem,
+        itemBuilder: (BuildContext context, int index) =>
+            ActivityCard(activities[index], index),
         itemCount: activities.length,
       );
     } else {
---
+++ lib/widgets/activities/activity_card.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+import './time.dart';
+
+class ActivityCard extends StatelessWidget {
+  final Map<String, dynamic> activity;
+  final int activityIndex;
+  ActivityCard(this.activity, this.activityIndex);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          Image.asset(activity['image'] ?? ''),
+          SizedBox(
+            height: 10.0,
+          ),
+          Container(
+            padding: EdgeInsets.only(top: 10.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Text(
+                  activity['title'] ?? '',
+                  style: TextStyle(
+                      fontSize: 26.0,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Oswald'),
+                ),
+                SizedBox(
+                  width: 8.0,
+                ),
+                TimeTag(activity['time'].toString()),
+              ],
+            ),
+          ),
+          DecoratedBox(
+            decoration: BoxDecoration(
+                border: Border.all(color: Theme.of(context).accentColor),
+                borderRadius: BorderRadius.circular(6.0)),
+            child: Padding(
+              child: Text('Hurry Up!'),
+              padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.5),
+            ),
+          ),
+          ButtonBar(
+            alignment: MainAxisAlignment.center,
+            children: <Widget>[
+              IconButton(
+                icon: Icon(Icons.info),
+                color: Theme.of(context).accentColor,
+                onPressed: () => Navigator.pushNamed<bool>(
+                      context,
+                      '/activity/' + activityIndex.toString(),
+                    ),
+              ),
+              IconButton(
+                icon: Icon(Icons.favorite_border),
+                color: Colors.red,
+                onPressed: () => Navigator.pushNamed<bool>(
+                      context,
+                      '/activity/' + activityIndex.toString(),
+                    ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
