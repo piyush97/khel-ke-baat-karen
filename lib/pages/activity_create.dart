578f75ebac2ea4a1f04d928fdf8eb8bb520a971e
Piyush Mehta <me@piyushmehta.com>
2019-04-15 17:13:30 +0530

    Modular for Modules :D

--- lib/pages/activity_create.dart
+++ lib/pages/activity_create.dart
@@ -16,47 +16,58 @@ class _ActivityCreatePageState extends State<ActivityCreatePage> {
   String _descriptionValue;
   double _time;
 
+  Widget _buildTitleTextField() {
+    return TextField(
+        decoration: InputDecoration(
+          labelText: 'Activity Name',
+        ),
+        autofocus: true,
+        onChanged: (String value) {
+          setState(() {
+            _titleValue = value;
+          });
+        });
+  }
+
+  Widget _buildDescriptionTextField() {
+    return TextField(
+      decoration: InputDecoration(
+        labelText: 'Activity Description',
+      ),
+      maxLines: 4,
+      onChanged: (String value) {
+        setState(() {
+          _descriptionValue = value;
+        });
+      },
+    );
+  }
+
+  Widget _buildTimeTextField() {
+    return TextField(
+      decoration: InputDecoration(
+        labelText: 'Activity Time',
+      ),
+      keyboardType: TextInputType.number,
+      autocorrect: true,
+      autofocus: true,
+      onChanged: (String value) {
+        setState(() {
+          _time = double.parse(value);
+        });
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.all(19.0),
       child: ListView(
         children: <Widget>[
-          TextField(
-            decoration: InputDecoration(
-              labelText: 'Activity Name',
-            ),
-            autofocus: true,
-            onChanged: (String value) {
-              setState(() {
-                _titleValue = value;
-              });
-            },
-          ),
-          TextField(
-            decoration: InputDecoration(
-              labelText: 'Activity Description',
-            ),
-            maxLines: 4,
-            onChanged: (String value) {
-              setState(() {
-                _descriptionValue = value;
-              });
-            },
-          ),
-          TextField(
-            decoration: InputDecoration(
-              labelText: 'Activity Time',
-            ),
-            keyboardType: TextInputType.number,
-            autocorrect: true,
-            autofocus: true,
-            onChanged: (String value) {
-              setState(() {
-                _time = double.parse(value);
-              });
-            },
-          ),
+          _buildTitleTextField(),
+          _buildDescriptionTextField(),
+          _buildTimeTextField(),
           SizedBox(
             height: 20.0,
           ),
