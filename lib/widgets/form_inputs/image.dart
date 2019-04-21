82980267f3e036da0942e597bc0e1a0143ee3815
Piyush Mehta <me@piyushmehta.com>
2019-04-22 00:40:21 +0530

    Image selectiong

--- lib/widgets/form_inputs/image.dart
+++ lib/widgets/form_inputs/image.dart
@@ -1,4 +1,3 @@
-
 import 'dart:io';
 
 import 'package:flutter/material.dart';
@@ -69,6 +68,25 @@ class _ImageInputState extends State<ImageInput> {
   @override
   Widget build(BuildContext context) {
     final buttonColor = Theme.of(context).primaryColor;
+    Widget previewImage = Text('Please select an image.');
+    if (_imageFile != null) {
+      previewImage = Image.file(
+        _imageFile,
+        fit: BoxFit.cover,
+        height: 300.0,
+        width: MediaQuery.of(context).size.width,
+        alignment: Alignment.topCenter,
+      );
+    } else if (widget.activity != null) {
+      previewImage = Image.network(
+        widget.activity.image,
+        fit: BoxFit.cover,
+        height: 300.0,
+        width: MediaQuery.of(context).size.width,
+        alignment: Alignment.topCenter,
+      );
+    }
+
     return Column(
       children: <Widget>[
         OutlineButton(
@@ -97,15 +115,16 @@ class _ImageInputState extends State<ImageInput> {
           ),
         ),
         SizedBox(height: 10.0),
-        _imageFile == null
-            ? Text('Please pick an image.')
-            : Image.file(
-                _imageFile,
-                fit: BoxFit.cover,
-                height: 300.0,
-                width: MediaQuery.of(context).size.width,
-                alignment: Alignment.topCenter,
-              )
+        previewImage
+        // _imageFile == null
+        // ? Text('Please pick an image.')
+        // : Image.file(
+        //     _imageFile,
+        //     fit: BoxFit.cover,
+        //     height: 300.0,
+        //     width: MediaQuery.of(context).size.width,
+        //     alignment: Alignment.topCenter,
+        //   )
       ],
     );
   }
