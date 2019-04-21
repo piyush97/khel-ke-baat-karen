6faafeb548f799b5fd14f7f1a0c1cdfbb6063b12
Piyush Mehta <me@piyushmehta.com>
2019-04-21 19:51:02 +0530

    tokenizing

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class User {
   final String id;
   final String email;
-  final String password;
+  final String token;
 
-  User({@required this.id, @required this.email, @required this.password});
+  User({@required this.id, @required this.email, @required this.token});
 }
