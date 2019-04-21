30327cf82a684383142302e66a5a5384559e7406
Piyush Mehta <me@piyushmehta.com>
2019-04-21 12:25:48 +0530

    cors init

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,14 @@
-const functions = require('firebase-functions');
-
+const functions = require("firebase-functions");
+const cors = require("cors")({ origin: true });
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+exports.storeImage = functions.https.onRequest((req, res) => {
+    return cors(req,res,() =>{
+        
+    });
+});
