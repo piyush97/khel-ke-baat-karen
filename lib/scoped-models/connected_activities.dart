fbfc50f4be59e780f813fcf564705457827a1a45
Piyush Mehta <me@piyushmehta.com>
2019-04-22 10:55:48 +0530

    api key

--- lib/scoped-models/connected_activities.dart
+++ lib/scoped-models/connected_activities.dart
@@ -1,14 +1,19 @@
 import 'dart:convert';
-import 'package:http/http.dart' as http;
-import 'package:scoped_model/scoped_model.dart';
 import 'dart:async';
 import 'dart:io';
+
 import 'dart:async' show Future;
-import 'dart:convert' show json;
+import 'package:flutter/services.dart' show rootBundle;
+import 'package:scoped_model/scoped_model.dart';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:rxdart/subjects.dart';
 import 'package:mime/mime.dart';
 import 'package:http_parser/http_parser.dart';
+
 import '../models/activity.dart';
 import '../models/user.dart';
+import '../models/auth.dart';
 
 class ConnectedActivitiesModel extends Model {
   List<Activity> _activities = [];
@@ -16,7 +21,6 @@ class ConnectedActivitiesModel extends Model {
   String _selActivityId;
   bool _isLoading = false;
 }
-
 // class SecretLoader {
 //   final String secretPath;
 
@@ -33,7 +37,6 @@ class ConnectedActivitiesModel extends Model {
 
 class ActivityModel extends ConnectedActivitiesModel {
   bool showFavorites = false;
-
   List<Activity> get allActivities {
     return List.from(_activities);
   }
@@ -70,50 +73,27 @@ class ActivityModel extends ConnectedActivitiesModel {
     return showFavorites;
   }
 
-  Future<Map<String, dynamic>> signup(String email, String password) async {
-    _isLoading = true;
-    notifyListeners();
-    final Map<String, dynamic> authData = {
-      'email': email,
-      'password': password,
-      'returnSecureToken': true
-    };
-
-    final http.Response response = await http.post(
-      'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=',
-      body: json.encode(authData),
-      headers: {'Content-Type': 'application/json'},
-    );
-    print(json.decode(response.body));
-    final Map<String, dynamic> responseData = json.decode(response.body);
-    bool hasError = true;
-    String message = 'Something went wrong.';
-    if (responseData.containsKey('idToken')) {
-      hasError = false;
-      message = 'Authentication succeeded!';
-    } else if (responseData['error']['message'] == 'EMAIL_EXISTS') {
-      message = 'This email already exists.';
-    }
-    _isLoading = false;
-    notifyListeners();
-    return {'success': !hasError, 'message': message};
-  }
-
   Future<Map<String, dynamic>> uploadImage(File image,
       {String imagePath}) async {
-    final mimneTypeData = lookupMimeType(image.path).split('/');
+    final mimeTypeData = lookupMimeType(image.path).split('/');
     final imageUploadRequest = http.MultipartRequest(
         'POST',
         Uri.parse(
-            ' https://us-central1-khel-ke-baat-karen.cloudfunctions.net/storeImage'));
-    final file = await http.MultipartFile.fromPath('image', image.path,
-        contentType: MediaType(mimneTypeData[0], mimneTypeData[1]));
+            'https://us-central1-khel-ke-baat-karen.cloudfunctions.net/storeImage'));
+    final file = await http.MultipartFile.fromPath(
+      'image',
+      image.path,
+      contentType: MediaType(
+        mimeTypeData[0],
+        mimeTypeData[1],
+      ),
+    );
     imageUploadRequest.files.add(file);
     if (imagePath != null) {
       imageUploadRequest.fields['imagePath'] = Uri.encodeComponent(imagePath);
     }
-    // imageUploadRequest.headers['Authorization'] =
-    //     'Bearer ${_authenticatedUser.token}';
+    imageUploadRequest.headers['Authorization'] =
+        'Bearer ${_authenticatedUser.token}';
 
     try {
       final streamedResponse = await imageUploadRequest.send();
@@ -145,16 +125,15 @@ class ActivityModel extends ConnectedActivitiesModel {
     final Map<String, dynamic> activityData = {
       'title': title,
       'description': description,
-      'image': 'http://images.huffingtonpost.com/2013-12-27-food12.jpg',
       'time': time,
-      'imagePath': uploadData['imagePath'],
-      'imageUrl': uploadData['imageUrl'],
       'userEmail': _authenticatedUser.email,
       'userId': _authenticatedUser.id,
+      'imagePath': uploadData['imagePath'],
+      'imageUrl': uploadData['imageUrl'],
     };
     try {
       final http.Response response = await http.post(
-          'https://khel-ke-baat-karen.firebaseio.com/activities.json',
+          'https://khel-ke-baat-karen.firebaseio.com/activities.json?auth=${_authenticatedUser.token}',
           body: json.encode(activityData));
       if (response.statusCode != 200 || response.statusCode != 201) {
         _isLoading = false;
@@ -167,6 +146,7 @@ class ActivityModel extends ConnectedActivitiesModel {
         title: title,
         description: description,
         image: uploadData['imageUrl'],
+        imagePath: uploadData['imagePath'],
         time: time,
         userEmail: _authenticatedUser.email,
         userId: _authenticatedUser.id,
@@ -187,26 +167,32 @@ class ActivityModel extends ConnectedActivitiesModel {
     // });
   }
 
-  void deleteActivities() {
+  Future<bool> deleteActivities() {
     _isLoading = true;
     final deletedActivityId = selectedActivity.id;
     _activities.removeAt(selectedActivityIndex);
     _selActivityId = null;
     notifyListeners();
-    http
+    return http
         .delete(
-            'https://khel-ke-baat-karen.firebaseio.com/activities/${deletedActivityId}.json')
+            'https://khel-ke-baat-karen.firebaseio.com/activities/${deletedActivityId}.json?auth=${_authenticatedUser.token}')
         .then((http.Response response) {
       _isLoading = false;
       notifyListeners();
+      return true;
+    }).catchError((error) {
+      _isLoading = false;
+      notifyListeners();
+      return false;
     });
   }
 
-  Future<bool> fetchActivities() {
+  Future<bool> fetchActivities({onlyForUser = false}) {
     _isLoading = true;
     notifyListeners();
     return http
-        .get('https://khel-ke-baat-karen.firebaseio.com/activities.json')
+        .get(
+            'https://khel-ke-baat-karen.firebaseio.com/activities.json?auth=${_authenticatedUser.token}')
         .then<Null>((http.Response response) {
       final List<Activity> fetchedActivityList = [];
       final Map<String, dynamic> activityListData = json.decode(response.body);
@@ -217,17 +203,25 @@ class ActivityModel extends ConnectedActivitiesModel {
       }
       activityListData.forEach((String activityId, dynamic activityData) {
         final Activity activity = Activity(
-          id: activityId,
-          title: activityData['title'],
-          description: activityData['description'],
-          image: activityData['image'],
-          time: activityData['time'],
-          userEmail: activityData['userEmail'],
-          userId: activityData['userId'],
-        );
+            id: activityId,
+            title: activityData['title'],
+            description: activityData['description'],
+            image: activityData['image'],
+            imagePath: activityData['imagePath'],
+            time: activityData['time'],
+            userEmail: activityData['userEmail'],
+            userId: activityData['userId'],
+            isFavorite: activityData['wishlistUsers'] == null
+                ? false
+                : (activityData['wishlistUsers'] as Map<String, dynamic>)
+                    .containsKey(_authenticatedUser.id));
         fetchedActivityList.add(activity);
       });
-      _activities = fetchedActivityList;
+      _activities = onlyForUser
+          ? fetchedActivityList.where((Activity activity) {
+              return activity.userId == _authenticatedUser.id;
+            }).toList()
+          : fetchedActivityList;
       _isLoading = false;
       notifyListeners();
       _selActivityId = null;
@@ -239,65 +233,101 @@ class ActivityModel extends ConnectedActivitiesModel {
   }
 
   Future<bool> updateActivities(
-      String title, String description, String image, double time) {
+      String title, String description, File image, double time) async {
     _isLoading = true;
+    notifyListeners();
+    String imageUrl = selectedActivity.image;
+    String imagePath = selectedActivity.imagePath;
+    if (image != null) {
+      final uploadData = await uploadImage(image);
+
+      if (uploadData == null) {
+        print('Upload failed!');
+        return false;
+      }
+
+      imageUrl = uploadData['imageUrl'];
+      imagePath = uploadData['imagePath'];
+    }
     final Map<String, dynamic> updateData = {
       'title': title,
       'description': description,
-      'image': 'http://images.huffingtonpost.com/2013-12-27-food12.jpg',
+      'imageUrl': imageUrl,
+      'imagePath': imagePath,
       'time': time,
       'userEmail': selectedActivity.userEmail,
-      'userId': selectedActivity.userId,
+      'userId': selectedActivity.userId
     };
-    return http
-        .put(
-      'https://khel-ke-baat-karen.firebaseio.com/activities/${selectedActivity.id}.json',
-      body: json.encode(updateData),
-    )
-        .then((http.Response response) {
+    try {
+      final http.Response response = await http.put(
+          'https://khel-ke-baat-karen.firebaseio.com/activities/${selectedActivity.id}.json?auth=${_authenticatedUser.token}',
+          body: json.encode(updateData));
       _isLoading = false;
       final Activity updatedActivity = Activity(
-        id: selectedActivity.id,
-        title: title,
-        description: description,
-        image: image,
-        time: time,
-        userEmail: selectedActivity.userEmail,
-        userId: selectedActivity.userId,
-      );
-      final int selectedActivityIndex =
-          _activities.indexWhere((Activity activity) {
-        return activity.id == _selActivityId;
-      });
+          id: selectedActivity.id,
+          title: title,
+          description: description,
+          image: imageUrl,
+          imagePath: imagePath,
+          time: time,
+          userEmail: selectedActivity.userEmail,
+          userId: selectedActivity.userId);
       _activities[selectedActivityIndex] = updatedActivity;
       notifyListeners();
       return true;
-    }).catchError((error) {
+    } catch (error) {
       _isLoading = false;
       notifyListeners();
       return false;
-    });
+    }
   }
 
-  void toggleFavActivity() {
+  void toggleActivityFavoriteStatus() async {
     final bool isCurrentlyFavorite = selectedActivity.isFavorite;
-    final bool newFavStatus = !isCurrentlyFavorite;
-    final Activity updatedActivity = Activity(
+    final bool newFavoriteStatus = !isCurrentlyFavorite;
+    final Activity updatedactivity = Activity(
         id: selectedActivity.id,
         title: selectedActivity.title,
         description: selectedActivity.description,
         time: selectedActivity.time,
         image: selectedActivity.image,
-        isFavorite: newFavStatus,
+        imagePath: selectedActivity.imagePath,
         userEmail: selectedActivity.userEmail,
-        userId: selectedActivity.userId);
-    _activities[selectedActivityIndex] = updatedActivity;
+        userId: selectedActivity.userId,
+        isFavorite: newFavoriteStatus);
+    _activities[selectedActivityIndex] = updatedactivity;
     notifyListeners();
+    http.Response response;
+    if (newFavoriteStatus) {
+      response = await http.put(
+        'https://khel-ke-baat-karen.firebaseio.com/activities/${selectedActivity.id}/wishlistUsers/${_authenticatedUser.id}.json?auth=${_authenticatedUser.token}',
+        body: json.encode(true),
+      );
+    } else {
+      response = await http.delete(
+          'https://khel-ke-baat-karen.firebaseio.com/activities/${selectedActivity.id}/wishlistUsers/${_authenticatedUser.id}.json?auth=${_authenticatedUser.token}');
+    }
+    if (response.statusCode != 200 && response.statusCode != 201) {
+      final Activity updatedactivity = Activity(
+          id: selectedActivity.id,
+          title: selectedActivity.title,
+          description: selectedActivity.description,
+          time: selectedActivity.time,
+          image: selectedActivity.image,
+          imagePath: selectedActivity.imagePath,
+          userEmail: selectedActivity.userEmail,
+          userId: selectedActivity.userId,
+          isFavorite: !newFavoriteStatus);
+      _activities[selectedActivityIndex] = updatedactivity;
+      notifyListeners();
+    }
   }
 
   void selectActivity(String activityId) {
     _selActivityId = activityId;
-    notifyListeners();
+    if (activityId != null) {
+      notifyListeners();
+    }
   }
 
   void toggleDisplayMode() {
@@ -307,11 +337,122 @@ class ActivityModel extends ConnectedActivitiesModel {
 }
 
 class UserModel extends ConnectedActivitiesModel {
-  void login(String email, String password) {
-    _authenticatedUser = User(
-      id: 'Piyush',
-      email: email,
-      password: password,
+  Timer _authTimer;
+  PublishSubject<bool> _userSubject = PublishSubject();
+  User get user {
+    return _authenticatedUser;
+  }
+
+  PublishSubject<bool> get userSubject {
+    return _userSubject;
+  }
+
+  Future<Map<String, dynamic>> authenticate(String email, String password,
+      [AuthMode mode = AuthMode.Login]) async {
+    _isLoading = true;
+    notifyListeners();
+    final Map<String, dynamic> authData = {
+      'email': email,
+      'password': password,
+      'returnSecureToken': true
+    };
+    String _loadApi() {
+      rootBundle.loadString('assets/secrets.json');
+      Map decoded = jsonDecode('assets/secrets.json');
+      return decoded['api_key'];
+    }
+      print(_loadApi.toString());
+
+    http.Response response;
+    if (mode == AuthMode.Login) {
+      response = await http.post(
+        'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=',
+        body: json.encode(authData),
+        headers: {'Content-Type': 'application/json'},
+      );
+    } else {
+      response = await http.post(
+        'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=',
+        body: json.encode(authData),
+        headers: {'Content-Type': 'application/json'},
+      );
+    }
+
+    final Map<String, dynamic> responseData = json.decode(response.body);
+    bool hasError = true;
+    String message = 'Something went wrong.';
+    print(responseData);
+    if (responseData.containsKey('idToken')) {
+      hasError = false;
+      message = 'Authentication succeeded!';
+      _authenticatedUser = User(
+          id: responseData['localId'],
+          email: email,
+          token: responseData['idToken']);
+      setAuthTimeOut(int.parse(responseData['expiresIn']));
+      final now = DateTime.now();
+      final DateTime expiryTime = now.add(
+        Duration(
+          seconds: int.parse(responseData['expiresIn']),
+        ),
+      );
+      _userSubject.add(true);
+
+      final SharedPreferences prefs = await SharedPreferences.getInstance();
+      prefs.setString('token', responseData['idToken']);
+      prefs.setString('userEmail', email);
+      prefs.setString('userId', responseData['localId']);
+      prefs.setString('expiryTime', expiryTime.toIso8601String());
+    } else if (responseData['error']['message'] == 'EMAIL_NOT_FOUND') {
+      message = 'This email was not found.';
+    } else if (responseData['error']['message'] == 'EMAIL_EXISTS') {
+      message = 'This email already exists.';
+    } else if (responseData['error']['message'] == 'INVALID_PASSWORD') {
+      message = 'The password is invalid.';
+    }
+    _isLoading = false;
+    notifyListeners();
+    return {'success': !hasError, 'message': message};
+  }
+
+  void autoAuthenticate() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    final String token = prefs.getString('token');
+    final String expiryTimeString = prefs.getString('expiryTime');
+    if (token != null) {
+      final DateTime now = DateTime.now();
+      final parsedExpiryTime = DateTime.parse(expiryTimeString);
+      if (parsedExpiryTime.isBefore(now)) {
+        _authenticatedUser = null;
+        notifyListeners();
+        return;
+      }
+      final String userEmail = prefs.getString('userEmail');
+      final String userId = prefs.getString('userId');
+      final tokenLifespan = parsedExpiryTime.difference(now).inSeconds;
+      _authenticatedUser = User(id: userId, email: userEmail, token: token);
+      _userSubject.add(true);
+      setAuthTimeOut(tokenLifespan);
+      notifyListeners();
+    }
+  }
+
+  void logout() async {
+    _authenticatedUser = null;
+    _authTimer.cancel();
+    _userSubject.add(false);
+  _selActivityId = null;
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.remove('token');
+    prefs.remove('userEmail');
+    prefs.remove('userId');
+    _userSubject.add(false);
+  }
+
+  void setAuthTimeOut(int time) {
+    _authTimer = Timer(
+      Duration(seconds: time),
+      logout,
     );
   }
 }
