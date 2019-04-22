3318fac0fc7d3dc06c4a88ccef68a8517e6447fd
Piyush Mehta <me@piyushmehta.com>
2019-04-22 09:20:19 +0530

    adding functionality

--- lib/widgets/activities/activity_fab.dart
+++ lib/widgets/activities/activity_fab.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import '../../models/activity.dart';
+import 'package:scoped_model/scoped_model.dart';
+import '../../scoped-models/main.dart';
 
 class ActivityFAB extends StatefulWidget {
+  final Activity activity;
+  ActivityFAB(this.activity);
+
   @override
   State<StatefulWidget> createState() {
     return _ActivityFABState();
@@ -10,43 +16,47 @@ class ActivityFAB extends StatefulWidget {
 class _ActivityFABState extends State<ActivityFAB> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: <Widget>[
-        Container(
-          height: 70.0,
-          width: 50.0,
-          alignment: FractionalOffset.topCenter,
-          child: FloatingActionButton(
-            backgroundColor: Theme.of(context).cardColor,
-            heroTag: 'contact',
-            mini: true,
-            onPressed: () {},
-            child: Icon(Icons.mail, color:Theme.of(context).primaryColor),
-          ),
-        ),
-        Container(
-          height: 70.0,
-          width: 50.0,
-          alignment: FractionalOffset.topCenter,
-          child: FloatingActionButton(
-            backgroundColor: Theme.of(context).cardColor,
-            heroTag: 'favorite',
-            mini: true,
-            onPressed: () {},
-            child: Icon(Icons.favorite,color:Colors.red),
-          ),
-        ),
-        Container(
-          height: 70.0,
-          width: 50.0,
-          child: FloatingActionButton(
-            heroTag: 'options',
-            onPressed: () {},
-            child: Icon(Icons.more_vert),
-          ),
-        ),
-      ],
+    return ScopedModelDescendant(
+      builder: (BuildContext context, Widget child, MainModel model) {
+        return Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Container(
+              height: 70.0,
+              width: 50.0,
+              alignment: FractionalOffset.topCenter,
+              child: FloatingActionButton(
+                backgroundColor: Theme.of(context).cardColor,
+                heroTag: 'contact',
+                mini: true,
+                onPressed: () {},
+                child: Icon(Icons.mail, color: Theme.of(context).primaryColor),
+              ),
+            ),
+            Container(
+              height: 70.0,
+              width: 50.0,
+              alignment: FractionalOffset.topCenter,
+              child: FloatingActionButton(
+                backgroundColor: Theme.of(context).cardColor,
+                heroTag: 'favorite',
+                mini: true,
+                onPressed: () {},
+                child: Icon(Icons.favorite, color: Colors.red),
+              ),
+            ),
+            Container(
+              height: 70.0,
+              width: 50.0,
+              child: FloatingActionButton(
+                heroTag: 'options',
+                onPressed: () {},
+                child: Icon(Icons.more_vert),
+              ),
+            ),
+          ],
+        );
+      },
     );
   }
 }
