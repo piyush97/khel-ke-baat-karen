12ffb446a392c224c51d698326269f7fc2bb22e4
Piyush Mehta <me@piyushmehta.com>
2019-04-23 22:28:34 +0530

    activity time store

--- lib/data/activity_repo.dart
+++ lib/data/activity_repo.dart
@@ -3,3 +3,13 @@ import 'dart:collection';
 import '../models/activity.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+
+class ActivityRepository {
+  // for time
+  static const ACTIVITY_ = "event";
+  static const String KEY_LAST_FETCH = "last_fetch";
+  static const int MILLISECONDS_IN_HOUR = 3600000;
+  static const int REFRESH_THRESHOLD = 3 * MILLISECONDS_IN_HOUR;
+
+}
+
