d91ba648a9ecae32c119495ce06b91a3c3cd787e
Piyush Mehta <me@piyushmehta.com>
2019-04-23 21:44:00 +0530

    Activity Db Method Made

--- lib/data/activity_db.dart
+++ lib/data/activity_db.dart
@@ -1,19 +1,46 @@
-import 'package:path/path.dart';
-import 'package:sqflite/sqflite.dart';
-
 import 'dart:async';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
 
 class ActivityDatabase {
-  static const EVENT_TABLE_NAME = "activity";
+  static const ACTIVITY_TABLE_NAME = "activity";
   static final ActivityDatabase _instance = ActivityDatabase._internal();
+
   factory ActivityDatabase() => _instance;
 
   static Database _db;
 
+  Future<Database> get db async {
+    if (_db != null) {
+      return _db;
+    }
+    _db = await initDB();
+    return _db;
+  }
+
   ActivityDatabase._internal();
 
-  Future<Database> initD() async {
+  Future<Database> initDB() async {
     var databasesPath = await getDatabasesPath();
     String path = join(databasesPath, "activity.db");
+    var theDatabase = await openDatabase(path, version: 1, onCreate: _onCreate);
+    return theDatabase;
+  }
+
+  void _onCreate(Database db, int version) async {
+    await db.execute("CREATE TABLE " +
+        ACTIVITY_TABLE_NAME +
+        " ("
+        "id STRING PRIMARY KEY, "
+        "name TEXT, "
+        "time INTEGER, "
+        "description TEXT, "
+        "url TEXT, "
+        "photo TEXT) ");
+  }
+
+  Future closeDb() async {
+    var dbClient = await db;
+    return dbClient.close();
   }
 }
