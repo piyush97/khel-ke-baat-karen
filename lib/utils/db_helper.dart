e83763641adb856e4d0a6e95ad446821582ff5a6
Piyush Mehta <me@piyushmehta.com>
2019-04-30 16:37:30 +0530

    query

--- lib/utils/db_helper.dart
+++ lib/utils/db_helper.dart
@@ -6,11 +6,24 @@ import '../models/activity_sqflite.dart';
 
 class DatabaseHelper {
   static DatabaseHelper _databaseHelper;
+  String activityTable = 'activity_table';
+  String colId = 'id';
+  String colTitle = 'title';
+  String colDescription = 'description';
+  String coltime = 'time';
+  String colimage = 'image';
+  String colimagePath = 'imagePath';
+  bool colisFavorite = false;
+
   DatabaseHelper._createInstance();
   factory DatabaseHelper() {
     if (_databaseHelper == null) {
       _databaseHelper = DatabaseHelper._createInstance();
     }
     return _databaseHelper;
+  }
+  void _createDb(Database db, int newVersion) async {
+    await db.execute(
+        'CREATE TABLE $activityTable($colId INTEGER PRIMARY KEY AUTOINCREMENT, $colTitle TEXT, $colDescription TEXT, $coltime TEXT,$colimage TEXT, $colimagePath TEXT ) ');
   }
 }
