4742fdf211c585df74f05d5f227831dea30fd3fe
Piyush Mehta <me@piyushmehta.com>
2019-04-30 12:22:40 +0530

    Draggable Game

---
+++ lib/pages/draggable_game.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+import 'package:chess_vectors_flutter/chess_vectors_flutter.dart';
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
+  GlobalKey<ScaffoldState> scaffoldKey = new GlobalKey();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: scaffoldKey,
+      appBar: AppBar(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Draggable(
+              data: 5,
+              child: Container(
+                width: 100.0,
+                height: 100.0,
+                child: Center(
+                  child: Text(
+                    "5",
+                    style: TextStyle(color: Colors.white, fontSize: 22.0),
+                  ),
+                ),
+                color: Colors.pink,
+              ),
+              feedback: Container(
+                width: 100.0,
+                height: 100.0,
+                child: Center(
+                  child: Text(
+                    "5",
+                    style: TextStyle(color: Colors.white, fontSize: 22.0),
+                  ),
+                ),
+                color: Colors.pink,
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Container(
+                  width: 100.0,
+                  height: 100.0,
+                  color: Colors.green,
+                  child: DragTarget(
+                    builder: (context, List<int> candidateData, rejectedData) {
+                      print(candidateData);
+                      return Center(
+                          child: Text(
+                        "Even",
+                        style: TextStyle(color: Colors.white, fontSize: 22.0),
+                      ));
+                    },
+                    onWillAccept: (data) {
+                      return true;
+                    },
+                    onAccept: (data) {
+                      if (data % 2 == 0) {
+                        scaffoldKey.currentState
+                            .showSnackBar(SnackBar(content: Text("Correct!")));
+                      } else {
+                        scaffoldKey.currentState
+                            .showSnackBar(SnackBar(content: Text("Wrong!")));
+                      }
+                    },
+                  ),
+                ),
+                Container(
+                  width: 100.0,
+                  height: 100.0,
+                  color: Colors.deepPurple,
+                  child: DragTarget(
+                    builder: (context, List<int> candidateData, rejectedData) {
+                      return Center(
+                          child: Text(
+                        "Odd",
+                        style: TextStyle(color: Colors.white, fontSize: 22.0),
+                      ));
+                    },
+                    onWillAccept: (data) {
+                      return true;
+                    },
+                    onAccept: (data) {
+                      if (data % 2 != 0) {
+                        scaffoldKey.currentState
+                            .showSnackBar(SnackBar(content: Text("Correct!")));
+                      } else {
+                        scaffoldKey.currentState
+                            .showSnackBar(SnackBar(content: Text("Wrong!")));
+                      }
+                    },
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
