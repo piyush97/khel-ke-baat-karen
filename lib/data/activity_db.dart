e5e31390da02a3a48573b81d9505191c1bc5a2f2
Piyush Mehta <me@piyushmehta.com>
2019-04-30 18:49:00 +0530

    Commented unused

--- lib/data/activity_db.dart
+++ lib/data/activity_db.dart
@@ -1,95 +1,95 @@
-import 'dart:async';
-import 'dart:io';
-import 'package:path/path.dart';
-import 'package:path_provider/path_provider.dart';
-import '../models/question_model.dart';
-import 'package:sqflite/sqflite.dart';
-import '../models/activity.dart';
+// import 'dart:async';
+// import 'dart:io';
+// import 'package:path/path.dart';
+// import 'package:path_provider/path_provider.dart';
+// import '../models/question_model.dart';
+// import 'package:sqflite/sqflite.dart';
+// import '../models/activity.dart';
 
-class activityDBProvider {
-  activityDBProvider._();
-  static final activityDBProvider db = activityDBProvider._();
+// class activityDBProvider {
+//   activityDBProvider._();
+//   static final activityDBProvider db = activityDBProvider._();
 
-  Database _database;
+//   Database _database;
 
-  Future<Database> get database async {
-    if (_database != null) return _database;
-    // if _database is null we instantiate it
-    _database = await initDB();
-    return _database;
-  }
+//   Future<Database> get database async {
+//     if (_database != null) return _database;
+//     // if _database is null we instantiate it
+//     _database = await initDB();
+//     return _database;
+//   }
 
-  initDB() async {
-    Directory documentsDirectory = await getApplicationDocumentsDirectory();
-    String path = join(documentsDirectory.path, "TestDB.db");
-    return await openDatabase(path, version: 1, onOpen: (db) {},
-        onCreate: (Database db, int version) async {
-      await db.execute("CREATE TABLE Questions ("
-          "q_title TEXT,"
-          "q_options TEXT,"
-          "answer TEXT,"
-          "image_file_path TEXT"
-          ")");
-      await db.execute("CREATE TABLE Rewards ("
-          "id INTEGER PRIMARY KEY,"
-          "image_path TEXT,"
-          "reward_points TEXT"
-          ")");
-    });
-  }
+//   initDB() async {
+//     Directory documentsDirectory = await getApplicationDocumentsDirectory();
+//     String path = join(documentsDirectory.path, "TestDB.db");
+//     return await openDatabase(path, version: 1, onOpen: (db) {},
+//         onCreate: (Database db, int version) async {
+//       await db.execute("CREATE TABLE Questions ("
+//           "q_title TEXT,"
+//           "q_options TEXT,"
+//           "answer TEXT,"
+//           "image_file_path TEXT"
+//           ")");
+//       await db.execute("CREATE TABLE Rewards ("
+//           "id INTEGER PRIMARY KEY,"
+//           "image_path TEXT,"
+//           "reward_points TEXT"
+//           ")");
+//     });
+//   }
 
-  newQuestion(Question newClient) async {
-    final db = await database;
-    var res = await db.insert("Questions", newClient.toMap());
-    return res;
-  }
+//   newQuestion(Question newClient) async {
+//     final db = await database;
+//     var res = await db.insert("Questions", newClient.toMap());
+//     return res;
+//   }
 
-  newReward(Activity newReward) async {
-    final db = await database;
-    var res = await db.insert("Rewards", newReward.toMap());
-    return res;
-  }
+//   newReward(Activity newReward) async {
+//     final db = await database;
+//     var res = await db.insert("Rewards", newReward.toMap());
+//     return res;
+//   }
 
-  // getQuestion(int id) async {
-  //   final db = await database;
-  //   var res = await db.query("Questions", where: "id = ?", whereArgs: [id]);
-  //   return res.isNotEmpty ? Question.fromMap(res.first) : null;
-  // }
+//   // getQuestion(int id) async {
+//   //   final db = await database;
+//   //   var res = await db.query("Questions", where: "id = ?", whereArgs: [id]);
+//   //   return res.isNotEmpty ? Question.fromMap(res.first) : null;
+//   // }
 
-  getReward(int id) async {
-    final db = await database;
-    var res = await db.query("Rewards", where: "id = ?", whereArgs: [id]);
-    return res.isNotEmpty ? Activity.fromMap(res.first) : null;
-  }
+//   getReward(int id) async {
+//     final db = await database;
+//     var res = await db.query("Rewards", where: "id = ?", whereArgs: [id]);
+//     return res.isNotEmpty ? Activity.fromMap(res.first) : null;
+//   }
 
-  updateReward(Activity newReward) async {
-    final db = await database;
-    var res = await db.update("Rewards", newReward.toMap(),
-        where: "id = ?", whereArgs: [newReward.id]);
-    return res;
-  }
+//   updateReward(Activity newReward) async {
+//     final db = await database;
+//     var res = await db.update("Rewards", newReward.toMap(),
+//         where: "id = ?", whereArgs: [newReward.id]);
+//     return res;
+//   }
 
-  Future<List<Question>> getAllQuestions() async {
-    final db = await database;
-    var res = await db.query("Questions");
+//   Future<List<Question>> getAllQuestions() async {
+//     final db = await database;
+//     var res = await db.query("Questions");
 
-    List<Question> list =
-        res.isNotEmpty ? res.map((c) => Question.fromMap(c)).toList() : [];
-    return list;
-  }
+//     List<Question> list =
+//         res.isNotEmpty ? res.map((c) => Question.fromMap(c)).toList() : [];
+//     return list;
+//   }
 
-  deleteClient(int id) async {
-    final db = await database;
-    return db.delete("Questions", where: "id = ?", whereArgs: [id]);
-  }
+//   deleteClient(int id) async {
+//     final db = await database;
+//     return db.delete("Questions", where: "id = ?", whereArgs: [id]);
+//   }
 
-  deleteReward(int id) async {
-    final db = await database;
-    return db.delete("Rewards", where: "id = ?", whereArgs: [id]);
-  }
+//   deleteReward(int id) async {
+//     final db = await database;
+//     return db.delete("Rewards", where: "id = ?", whereArgs: [id]);
+//   }
 
-  deleteAll() async {
-    final db = await database;
-    db.rawDelete("Delete * from Questions");
-  }
-}
+//   deleteAll() async {
+//     final db = await database;
+//     db.rawDelete("Delete * from Questions");
+//   }
+// }
