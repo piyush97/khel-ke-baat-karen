9e7dc0a3daaba400e1057cd92471a6e1e12f833b
Piyush Mehta <me@piyushmehta.com>
2019-05-01 23:25:40 +0530

    color game made

--- lib/pages/color_game.dart
+++ lib/pages/color_game.dart
@@ -9,23 +9,28 @@ class ColorGame extends StatefulWidget {
 }
 
 class ColorGameState extends State<ColorGame> {
+  // Map to keep track of score
   final Map<String, bool> score = {};
-  final Map choice = {
+
+  /// Choices for game
+  final Map choices = {
     '🍏': Colors.green,
     '🍋': Colors.yellow,
     '🍅': Colors.red,
     '🍇': Colors.purple,
-    '🍠': Colors.brown,
-    '🥕': Colors.orange,
+    '🥥': Colors.brown,
+    '🥕': Colors.orange
   };
-  // Random seed to shuffle order of items
+
+  // Random seed to shuffle order of items.
   int seed = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Score ${score.length}/6'),
-      ),
+          title: Text('Score ${score.length} / 6'),
+          backgroundColor: Colors.pink),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.refresh),
         onPressed: () {
@@ -36,14 +41,78 @@ class ColorGameState extends State<ColorGame> {
         },
       ),
       body: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: <Widget>[
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
           Column(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               crossAxisAlignment: CrossAxisAlignment.end,
-              children: choices.key.map((emoji) {}).toList()),
+              children: choices.keys.map((emoji) {
+                return Draggable<String>(
+                  data: emoji,
+                  child: Emoji(emoji: score[emoji] == true ? '✅' : emoji),
+                  feedback: Emoji(emoji: emoji),
+                  childWhenDragging: Emoji(emoji: '🌱'),
+                );
+              }).toList()),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children:
+                choices.keys.map((emoji) => _buildDragTarget(emoji)).toList()
+                  ..shuffle(Random(seed)),
+          )
         ],
       ),
     );
   }
+
+  Widget _buildDragTarget(emoji) {
+    return DragTarget<String>(
+      builder: (BuildContext context, List<String> incoming, List rejected) {
+        if (score[emoji] == true) {
+          return Container(
+            color: Colors.white,
+            child: Text('Correct!'),
+            alignment: Alignment.center,
+            height: 80,
+            width: 200,
+          );
+        } else {
+          return Container(color: choices[emoji], height: 80, width: 200);
+        }
+      },
+      onWillAccept: (data) => data == emoji,
+      onAccept: (data) {
+        setState(() {
+          score[emoji] = true;
+          plyr.play('success.mp3');
+        });
+      },
+      onLeave: (data) {},
+    );
+  }
 }
+
+class Emoji extends StatelessWidget {
+  Emoji({Key key, this.emoji}) : super(key: key);
+
+  final String emoji;
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.transparent,
+      child: Container(
+        alignment: Alignment.center,
+        height: 50,
+        padding: EdgeInsets.all(10),
+        child: Text(
+          emoji,
+          style: TextStyle(color: Colors.black, fontSize: 50),
+        ),
+      ),
+    );
+  }
+}
+
+AudioCache plyr = AudioCache();
